333afbc6ce7b41f97852c30c06672155bd52d723
GuyRinat <guyr09@gmail.com>
2017-09-25 04:02:54 +0300

    Restoring code since the deleting

---
+++ .vscode/GameObjects.py
@@ -0,0 +1,153 @@
+class ChessPiece:
+
+    chessPiecesCounter = 0
+    
+    def __init__(self, char = '', color = ''):
+        self.__character = char
+        self.__color = color
+        self.__isAlive = True
+        ChessPiece.chessPiecesCounter += 1
+
+    @property
+    def char(self):
+        return self.__character
+    @char.setter
+    def char(self, value):
+        self.__character = value
+
+    @property
+    def color(self):
+        return self.__color
+    @color.setter
+    def color(self, value):
+        self.__color = value
+    
+    @property
+    def isAlive(self):
+        return self.__isAlive
+    @isAlive.setter
+    def isAlive(self, value):
+        self.__isAlive = value
+
+    def __str__(self):
+        return '({0},{1},{2})'.format(self.__character, self.__color, 'Alive' if self.__isAlive else 'Dead')
+
+class ChessBoard:
+
+    SQRS_IN_ROW = 8
+    WHITE_PAWNS_ROW = 1
+    BLACK_PAWNS_ROW = 6
+    WHITE_NOBILITY_ROW = 0
+    BLACK_NOBILITY_ROW = 7
+
+    def __init__(self):
+        self.positionsTable = []
+        self.initPositions()
+
+    def initPositions(self):
+        for i in range(ChessBoard.SQRS_IN_ROW):
+            row = []
+            for j in range(ChessBoard.SQRS_IN_ROW):
+                row.append(Position(i+1,j+1))
+            self.positionsTable.append[row]
+
+    def initCharStartingPositions(self, blackObjects, whiteObjects):
+        if type(blackObjects) != list or type(whiteObjects) != list : return
+        for i in range(ChessBoard.SQRS_IN_ROW):
+            Position(self.positionsTable[ChessBoard.WHITE_PAWNS_ROW][i]).chessPiece = ChessPiece("Pawn", 'White')
+            list(whiteObjects).append(Position(self.positionsTable[ChessBoard.WHITE_PAWNS_ROW][i]).chessPiece)
+        for i in range(ChessBoard.SQRS_IN_ROW):
+            Position(self.positionsTable[ChessBoard.WHITE_NOBILITY_ROW][i]).chessPiece = {
+                0 : ChessPiece('Rook', 'White'),
+                1 : ChessPiece('Knight', 'White'),
+                2 : ChessPiece('Bishop', 'White'),
+                3 : ChessPiece('Queen', 'White'),
+                4 : ChessPiece('King', 'White'),
+                5 : ChessPiece('Bishop', 'White'),
+                6 : ChessPiece('Knight', 'White'),
+                7 : ChessPiece('Rook', 'White')
+            }[i]
+            list(whiteObjects).append(Position(self.positionsTable[ChessBoard.WHITE_NOBILITY_ROW][i]).chessPiece)
+        for i in range(ChessBoard.SQRS_IN_ROW):
+                Position(self.positionsTable[ChessBoard.BLACK_PAWNS_ROW][i]).chessPiece = ChessPiece("Pawn", 'Black')
+                list(blackObjects).append(Position(self.positionsTable[ChessBoard.BLACK_PAWNS_ROW][i]).chessPiece)
+        for i in range(ChessBoard.SQRS_IN_ROW):
+            Position(self.positionsTable[ChessBoard.BLACK_NOBILITY_ROW][i]).chessPiece = {
+                0 : ChessPiece('Rook', 'Black'),
+                1 : ChessPiece('Knight', 'Black'),
+                2 : ChessPiece('Bishop', 'Black'),
+                3 : ChessPiece('Queen', 'Black'),
+                4 : ChessPiece('King', 'Black'),
+                5 : ChessPiece('Bishop', 'Black'),
+                6 : ChessPiece('Knight', 'Black'),
+                7 : ChessPiece('Rook', 'Black')
+            }[i]
+            list(blackObjects).append(Position(self.positionsTable[ChessBoard.BLACK_NOBILITY_ROW][i]).chessPiece)
+    
+    def countAliveByColor(self, color):
+        counter = 0
+        for i in range(ChessBoard.SQRS_IN_ROW):
+            for j in range(ChessBoard.SQRS_IN_ROW):
+                if ChessPiece(Position(self.positionsTable[i][j]).chessPieces).color.__eq__(color) and ChessPiece(Position(self.positionsTable[i][j]).chessPiece).isAlive:
+                    counter += 1
+        return counter
+
+    def isKingByColorAlive(self, color):
+        aliveKing = ChessPiece('King', color, True)
+        for i in range(ChessBoard.SQRS_IN_ROW):
+            for j in range(ChessBoard.SQRS_IN_ROW):
+                if not Position(self.positionsTable[i][j]).chessPieces.__eq__(aliveKing):
+                    return False
+        return True
+
+    def isPositionInBounds(self, position):
+        if not Position.isValid(position):
+            return
+        return Position(position).x > 0 and Position(position).x < ChessBoard.SQRS_IN_ROW and Position(position).y > 0 and Position(position).y < ChessBoard.SQRS_IN_ROW
+
+    def objectsBetween(self, pos1, pos2):
+        if not Position.isValid(pos1) or not Position.isValid(pos2) or pos1 == pos2
+            return
+        x1 = Position(pos1).x
+        y1 = Position(pos1).y
+        x2 = Position(pos2).x
+        y2 = Position(pos2).y
+        xDiff = x2 - x1
+        yDiff = y2 - y1
+        if xDiff == yDiff:
+            for i in range(x1+1,x2-1):
+                if not Position(self.positionsTable[i][i]).isEmpty():
+                    return True
+        elif xDiff > 0 and yDiff == 0:
+            for i in range(x1+1,x2-1):
+                if not Position(self.positionsTable[i][y1]).isEmpty():
+                    return True
+        elif yDiff > 0 and xDiff == 0:
+            for i in range(y1+1,y2-1):
+                if not Position(self.positionsTable[x1][i]).isEmpty():
+                    return True
+        return False
+    
+    def printBoard(self):
+        for i in range(ChessBoard.SQRS_IN_ROW):
+            row = ''
+            for j in range(ChessBoard.SQRS_IN_ROW):
+                line += self.positionsTable[i,j]
+            print(row)
+
+class Position:
+
+    def __init__(self, x=-1, y=-1):
+        self.x = x
+        self.y = y
+        self.chessPiece = None
+
+    def isEmpty(self):
+        return self.chessPiece == None
+
+    @classmethod
+    def isValid(self, position):
+        return position and type(position) == Position
+
+    def __str__(self):
+        return '({0},{1})'.format(self.x, self.y)
---
+++ .vscode/GameRunner.py
@@ -0,0 +1,62 @@
+from GameObjects import *
+
+class GameMangaer:
+    
+    Colors = ('Black', 'White')
+
+    def __init__(self):
+        self.chessBoard = ChessBoard()
+        self.turnsCounter = 1
+
+    def playTurn(self):
+        while not self.hasFinished():
+            if self.turnsCounter == 1: 
+                self.chessBoard.initCharStartingPositions()
+            if(self.turnsCounter % 2 == 0):
+                print("White's Turn.")
+            else: 
+                print("Black's Turn.")
+            self.turnsCounter += 1
+            self.makeMove()   
+
+    def hasFinished(self):
+        for color in GameMangaer.Colors:
+            if not self.chessBoard.isKingByColorAlive(color):
+                print('{0} Lost!'.format(color))
+                return True
+        return False
+
+    def makeMove(self, pos1, pos2):
+        if not self.canMove(pos1,pos2):
+             return
+        if Position(pos2).chessPiece != None:
+            ChessPiece(Position(pos2).chessPiece).isAlive = False
+        Position(pos2).chessPiece = Position(pos1).chessPiece
+        Position(pos1).chessPiece = None
+     
+    def canMove(self, pos1, pos2):
+        if Position.isValid(pos1) or Position.isValid(pos2) or pos1 == pos2:
+            return False
+        elif ChessPiece(Position(pos1).chessPiece).color.__eq__(ChessPiece(Position(pos2).chessPiece).color):
+            print('Pieces From The Same Team!')
+            return
+        elif not self.chessBoard.isPositionInBounds(pos1) or not self.chessBoard.inBounds(pos2):
+            print('Positions Are Out Of Bounds Of The Board')
+            return
+        x1 = Position(pos1).x
+        y1 = Position(pos1).y
+        x2 = Position(pos2).x
+        y2 = Position(pos2).y
+        if ChessPiece(Position(pos1).chessPiece).char == 'Pawn':
+            return {
+                'Black' : y2 = y1 + 1,
+                'White' : y2 = y1 - 1
+            }[ChessPiece(Position(pos1).chessPiece).color] and (abs(x2-x1) == 0 and Position(pos2).chessPiece == None or abs(x2-x1) == 1 and Position(pos2).chessPiece != None)
+        else:
+            return {
+                'King' : (0,1,2).__contains__(abs(y2-y1+x2-x1)) and 
+                'Queen' : not self.chessBoard.objectsBetween(pos1, pos2) and 
+                'Bishop' : not self.chessBoard.objectsBetween(pos1, pos2) and 
+                'Knight' : 
+                'Rook' : not self.chessBoard.objectsBetween(pos1, pos2) and 
+            }[ChessPiece(Position(pos1).chessPiece).char]
